1295b63966435743331a998b12b7c0d9225633f2
Maxime Cordeiro <maxime.cordeiro@avec.fr>
2022-06-29 16:30:32 +0200

    update changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,16 +5,34 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [unreleased]
+## [0.0.2] - 2022-06-29
+
+### Changed
+
+- CHANGELOG.md
+- App.tsx
 
 ### Added
 
+- routes/NoteStack
+- routes/TabRoute
+- screens/HomeScreen
+- screens/MyNoteScreen
+- screens/NewNoteScreen
+- service/noteApi
+- utils/axios
+- utils/DefaultStyles
+- interfaces/not
 - doc api routes
 - doc consigne projet
 - doc folder
-- axios
+- axios package
 - env file
 - react navigation
+
+## [unreleased]
+
+### Added
 
 ### Changed
 
@@ -32,5 +50,6 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - create expo app
 - init the repository
 
-[unreleased]: https://github.com/LouisPLN/RN-Learning-Notes/compare/v0.0.1...develop
+[unreleased]: https://github.com/LouisPLN/RN-Learning-Notes/compare/v0.0.2...develop
+[0.0.2]: https://github.com/LouisPLN/RN-Learning-Notes/releases/tag/v.0.0.2
 [0.0.1]: https://github.com/LouisPLN/RN-Learning-Notes/releases/tag/v.0.0.1
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,12 +1,29 @@
 import { View, Text, SafeAreaView } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 
 // components
 import useStyles from "../utils/DefaultStyles";
+// services
+import { getAllNotes, updateNoteById, postNote } from "@services/noteApi";
+// interface
+import { INote } from "utils/interfaces/note";
 
 const HomeScreen = () => {
   const styles = useStyles();
+
+  // constants
+  const [notesList, setNotesList] = useState([]);
+  // functions
+  const getNotes = async () => {
+    const allNotes = await getAllNotes();
+    setNotesList(allNotes);
+  };
+
+  // initialization
+  useEffect(() => {
+    getNotes();
+  }, []);
 
   return (
     <SafeAreaView style={styles.all}>
@@ -15,13 +32,14 @@ const HomeScreen = () => {
           <Text style={styles.title}>📌 Les notes partagés</Text>
         </View>
         <View style={styles.notesContainer}>
-          <View style={styles.note}>
-            <View style={styles.borderColor}></View>
-            <Text style={styles.noteText}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum,
-              qui!
-            </Text>
-          </View>
+          {notesList?.map((note: INote, index: React.Key) => {
+            return (
+              <View key={index} style={styles.note}>
+                <View style={styles.borderColor}></View>
+                <Text style={styles.noteText}>{note.title}</Text>
+              </View>
+            );
+          })}
         </View>
       </View>
     </SafeAreaView>
