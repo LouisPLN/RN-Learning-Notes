0d21024ebda48da45318f26c1a5bb5f31e5091ac
Louis <louispoulinpro@gmail.com>
2022-06-29 10:00:09 +0200

    Config babel export module

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,29 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['.'],
+          extensions: ['.tsx', '.ts', '.json', '.svg', '.jpg'],
+          alias: {
+            '@assets': './src/assets',
+            '@components': './src/components',
+            '@atoms': './src/components/atoms',
+            '@molecules': './src/components/molecules',
+            '@organisms': './src/components/organisms',
+            '@screens': './src/screens',
+            '@services': './src/services',
+            '@utils': './src/utils',
+            '@routes': './src/routes',
+            // '@interfaces': './src/interfaces',
+            // '@routes': './src/routes',
+            // '@stores': './src/stores',
+            // '@styles': './src/styles',
+          },
+        },
+      ],
+    ]
   };
 };
